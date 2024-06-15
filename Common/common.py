a2a0e2144be513e1c1159473f261a62f66578fd5
Max Lenk <maxlenk@gmail.com>
2024-06-14 20:43:04 -0700

    initial config/common files. closes #9

---
+++ Common/common.py
@@ -0,0 +1,17 @@
+"""
+Global definitions of hard coded variables.
+"""
+
+# Models
+MODEL_DNN = 'dnn'
+
+# Memory Interfaces
+MEMORY_ORIGINAL_GRAVES2014 = 'NTM graves 2014'
+MEMORY_LIE_ACCESS_YANG2014 = 'LANTM yang 2014'
+
+# Hyperparameters
+HYPERPARAM_LEARNING_RATE = 'learning_rate'
+HYPERPARAM_BATCH_SIZE = 'batch size'
+HYPERPARAM_OPTIMIZER = 'optimizer'
+HYPERPARAM_OPTIMIZER_ADAM = 'adam'
+
