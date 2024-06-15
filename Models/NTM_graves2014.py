ed329afba8a34db8aa5e637cda68fe3930091324
Max Lenk <maxlenk@gmail.com>
2024-06-15 01:17:00 -0700

    merging

--- Models/NTM_graves2014.py
+++ Models/NTM_graves2014.py
@@ -1,28 +1,32 @@
 import jax
 from flax import linen as nn
 
-from Common.common import *
+from Common import common
 
 
+# TODO may need some extra stuff about hidden state learning?
 class LSTMModel(nn.Module):
-    """Basic stacked LSTM for controlling the NTM"""
+    """Basic stacked LSTM for controlling an NTM"""
+
     features: int
     layers: int
 
     def setup(self):
-        lstm_layer = nn.scan(nn.OptimizedLSTMCell,
-                               variable_broadcast="params",
-                               split_rngs={"params": False},
-                               in_axes=1, out_axes=1,
-                               reverse=False)
+        lstm_layer = nn.scan(
+            nn.OptimizedLSTMCell,
+            variable_broadcast="params",
+            split_rngs={"params": False},
+            in_axes=1,
+            out_axes=1,
+        )
         self.lstm_layers = [lstm_layer(self.features) for _ in range(self.layers)]
-        
-    @nn.remat    
+
+    @nn.remat
     def __call__(self, x):
         for lstm_layer in self.lstm_layers:
-            carry, hidden = lstm_layer.initialize_carry(jax.random.PRNGKey(RANDOM_SEED),x[:, 0].shape)
+            carry, hidden = lstm_layer.initialize_carry(
+                jax.random.PRNGKey(common.RANDOM_SEED), x[:, 0].shape
+            )
             (carry, hidden), x = lstm_layer((carry, hidden), x)
 
         return x
-    
-
