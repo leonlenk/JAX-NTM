ac243b151173e36ddb31c9e7a60a69e6fa54b18b
Max Lenk <maxlenk@gmail.com>
2024-06-16 07:48:58 -0700

    #5 graves 2014 memory cleanup and circular conv

--- Memories/NTM_graves2014.py
+++ Memories/NTM_graves2014.py
@@ -6,7 +6,7 @@ import optax
 
 
 class Memory(nn.Module):
-    """Memory interface for NTM (Graves 2014).
+    """Memory interface for NTM from Graves 2014 (arXiv:1410.5401).
     Memory has a size of: batch size x N x M.
     N = number of memory locations
     N = size of vector at each memory location
@@ -40,7 +40,7 @@ class Memory(nn.Module):
     def write(
         self, read_weights, erase_vector, add_vector
     ):  # TODO - figure out all the necessary squeezing/unsqueezing
-        """warXiv:1410.5401 section 3.2"""
+        """arXiv:1410.5401 section 3.2"""
 
         # calculate erase and add vectors
         erase = jnp.matmul(read_weights.unsqueeze(-1), erase_vector.unsqueeze(1))
@@ -63,7 +63,7 @@ class Memory(nn.Module):
 
         # location addressing
         gate_weights = self._interpolate(
-            previous_weights, content_weights, interp_gate_scalar
+            content_weights, previous_weights, interp_gate_scalar
         )
 
         # calculate and sharpen weights
@@ -72,29 +72,38 @@ class Memory(nn.Module):
 
         return sharpened_weights
 
-    def _similarity(self, k, beta):
+    def _similarity(self, key_vector, key_strength):
         """arXiv:1410.5401 equation 6"""
-        k = k.view(self.batch_size, 1, -1)
+        key_vector = key_vector.view(self.batch_size, 1, -1)
         w = nn.softmax(
-            beta * optax.cosine_similarity(self.memory, k, epsilon=1e-16), axis=1
+            key_strength
+            * optax.cosine_similarity(self.memory, key_vector, epsilon=1e-16),
+            axis=1,
         )
         return w
 
-    def _interpolate(self, w_prev, wc, g):
-        return g * wc + (1 - g) * w_prev
+    def _interpolate(self, weights, previous_weights, interp_gate_scalar):
+        """arXiv:1410.5401 equation 7"""
+        return (
+            interp_gate_scalar * weights + (1 - interp_gate_scalar) * previous_weights
+        )
 
-    def _shift(self, wg, s):
-        result = jnp.zeros(wg.size())
+    def _shift(self, gated_weighting, shift_weighting):
+        """arXiv:1410.5401 equation 8"""
+        result = jnp.zeros(gated_weighting.size())
         for b in range(self.batch_size):
-            result[b] = circular_convolution(wg[b], s[b])
+            result[b] = circular_convolution_1d(gated_weighting[b], shift_weighting[b])
         return result
 
-    def _sharpen(self, ŵ, γ):
-        w = ŵ**γ
+    def _sharpen(self, weights, sharpen_scalar):
+        """arXiv:1410.5401 equation 9"""
+        w = weights**sharpen_scalar
         w = jnp.divide(w, jnp.sum(w, axis=1).view(-1, 1) + 1e-16)
         return w
 
 
-def circular_convolution(w, s):
-    # TODO implement circular convolution
-    return
+def circular_convolution_1d(array, kernel):
+    assert kernel.shape[0] % 2 == 1
+    pad_length = kernel.shape[0] // 2
+    padded_array = jnp.pad(array, pad_length, "wrap")
+    return jnp.correlate(padded_array, kernel, mode="valid")
