48b1672dc272ddeaee85cd2ed2cb481d93a02b3c
Max Lenk <maxlenk@gmail.com>
2024-06-24 21:50:41 +0200

    graves 2014 memory tests, train loop typing

--- Training/training_loop.py
+++ Training/training_loop.py
@@ -8,27 +8,29 @@ from Common import common
 
 
 def train(
-    wandb_conf: dict,
+    train_config: dict,
     state: ArrayLike,
-    train_step: Callable,
+    train_step: Callable[[ArrayLike, ArrayLike, dict], tuple[ArrayLike, ArrayLike]],
     train_dataset: Iterable,
     val_dataset: Iterable | None = None,
     wandb_tags: list[str] = [],
-):
-    assert common.CONFIG_EPOCHS in wandb_conf, "Number of epochs required"
+) -> ArrayLike:
+    assert (
+        common.CONFIG_EPOCHS in train_config
+    ), "The number of epochs to train is a required config"
 
     wandb.init(
         project=common.WANDB_PROJECT_NAME,
         job_type=common.WANDB_JOB_TRAIN,
-        config=wandb_conf,
+        config=train_config,
         tags=wandb_tags,
     )
 
-    for epoch in tqdm(range(1, wandb_conf[common.CONFIG_EPOCHS] + 1)):
+    for epoch in tqdm(range(1, train_config[common.CONFIG_EPOCHS] + 1)):
         # TODO add function to convert list of metrics (loss, accuracy, etc) into a loggable piece of information
         train_metrics = []
         for data in tqdm(train_dataset):
-            state, metrics = train_step(state, data)
+            state, metrics = train_step(state, data, train_config)
             train_metrics.append(metrics)
 
         # TODO add validation loop. Check if val_dataset is none. Add config option for how often to do validation?
@@ -39,6 +41,8 @@ def train(
         # wandb.log({
         #     "Train Loss": loss,
         # }, step=epoch)
+
+    return state
 
 
 # TODO add "evaluate" (aka "inference"). Add "test" separately?
