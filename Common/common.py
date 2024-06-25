f4d61c7ca6d17953341c4c64f5d179c919a66638
Max Lenk <maxlenk@gmail.com>
2024-06-25 20:23:18 +0200

    adding val and metrics to train loop, misc cleanup

--- Common/common.py
+++ Common/common.py
@@ -18,17 +18,22 @@ GRAVES2014_LSTM_LAYER_STATE = "lstm_layer_state_"
 GRAVES2014_READ_CONTROLLER_BIAS = "read_controller_bias_"
 YANG2014 = "LANTM yang 2014"
 
+# Visualization
+VISUALIZATION_OUTPUT_DIR = "Visualization_Outputs/"
+VISUALIZATION_GIF_EXTENSION = ".gif"
+VISUALIZATION_IMG_EXTENSION = ".png"
+
 # Training Configuration (Hyperparameters)
 CONFIG_EPOCHS = "epochs"
 CONFIG_LEARNING_RATE = "learning_rate"
 CONFIG_BATCH_SIZE = 16
 CONFIG_OPTIMIZER = "optimizer"
 CONFIG_OPTIMIZER_ADAM = "adam"
+CONFIG_VAL_PERIOD = "validation_period_in_epochs"
 
-# Visualization
-VISUALIZATION_OUTPUT_DIR = "Visualization_Outputs/"
-VISUALIZATION_GIF_EXTENSION = ".gif"
-VISUALIZATION_IMG_EXTENSION = ".png"
+# Metrics
+METRIC_LOSS = "loss"
+METRIC_ACCURACY = "accuracy"
 
 # WandB
 WANDB_PROJECT_NAME = "neural-turing-machines"
@@ -39,3 +44,6 @@ WANDB_TAG_TRANSFORMER = "transformer"
 WANDB_TAG_RNN = "rnn"
 WANDB_TAG_FNN = "fnn"
 WANDB_TAG_LIE_ACCESS = "lie access"
+WANDB_LOG_TRAIN = "train"
+WANDB_LOG_VAL = "val"
+WANDB_LOG_TEST = "test"
--- Training/training_loop.py
+++ Training/training_loop.py
@@ -1,5 +1,6 @@
 from typing import Callable, Iterable
 
+import jax.numpy as jnp
 import wandb
 from jax.typing import ArrayLike
 from tqdm import tqdm
@@ -7,17 +8,35 @@ from tqdm import tqdm
 from Common import common
 
 
+def average_metric(metrics: list[dict]) -> dict:
+    assert len(metrics) > 0, "Must have at least one metric to aggregate"
+    averaged_metric = {}
+    for key in metrics[0]:
+        averaged_metric[key] = jnp.average(
+            jnp.array([metric[key] for metric in metrics])
+        )
+
+    return averaged_metric
+
+
 def train(
     train_config: dict,
     state: ArrayLike,
-    train_step: Callable[[ArrayLike, ArrayLike, dict], tuple[ArrayLike, ArrayLike]],
+    train_step: Callable[[ArrayLike, ArrayLike, dict], tuple[ArrayLike, dict]],
     train_dataset: Iterable,
     val_dataset: Iterable | None = None,
+    val_step: Callable[[ArrayLike, ArrayLike, dict], dict] | None = None,
+    metric_aggregator: Callable[[list[dict]], dict] = average_metric,
     wandb_tags: list[str] = [],
 ) -> ArrayLike:
     assert (
         common.CONFIG_EPOCHS in train_config
     ), "The number of epochs to train is a required config"
+
+    if val_dataset is not None or val_step is not None:
+        assert (
+            val_dataset is not None and val_step is not None
+        ), "Both val_dataset and val_step are required to perform validation"
 
     wandb.init(
         project=common.WANDB_PROJECT_NAME,
@@ -27,20 +46,47 @@ def train(
     )
 
     for epoch in tqdm(range(1, train_config[common.CONFIG_EPOCHS] + 1)):
-        # TODO add function to convert list of metrics (loss, accuracy, etc) into a loggable piece of information
-        train_metrics = []
+        # train the model on each batch
+        train_metrics: list[dict] = []
         for data in tqdm(train_dataset):
+            # run the train step function
             state, metrics = train_step(state, data, train_config)
+            # record the results
             train_metrics.append(metrics)
 
-        # TODO add validation loop. Check if val_dataset is none. Add config option for how often to do validation?
+        # combine the metrics from each batch into a single dictionary to log
+        train_metric = metric_aggregator(train_metrics)
+        wandb.log(
+            {
+                f"{common.WANDB_LOG_TRAIN}_{key}": train_metric[key]
+                for key in train_metric
+            },
+            step=epoch,
+        )
 
-        # TODO save best model based on validation results (use single-valued metric function?)
+        # perform validation if desired
+        if val_dataset is not None and val_step is not None:
+            if (
+                common.CONFIG_VAL_PERIOD not in train_config
+                or epoch % train_config[common.CONFIG_VAL_PERIOD] == 0
+            ):
+                val_metrics: list[dict] = []
+                for data in tqdm(train_dataset):
+                    # run the val step function
+                    metrics = val_step(state, data, train_config)
+                    # record the results
+                    val_metrics.append(metrics)
+                # combine the metrics from each batch into a single dictionary to log
+                val_metric = metric_aggregator(val_metrics)
+                wandb.log(
+                    {
+                        f"{common.WANDB_LOG_VAL}_{key}": val_metric[key]
+                        for key in val_metric
+                    },
+                    step=epoch,
+                )
 
-        # TODO add wandb.log
-        # wandb.log({
-        #     "Train Loss": loss,
-        # }, step=epoch)
+        # TODO save best model based on validation results
 
     return state
 
