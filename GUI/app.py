115b5a1e893ef49b3ffca736fe85175f46c4100b
leon <leonlenk@gmail.com>
2024-07-22 22:30:13 -0600

    add flask boiler plate and nav bar

---
+++ GUI/app.py
@@ -0,0 +1,14 @@
+# run with: flask --app GUI.app run --debug
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("pages/index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
